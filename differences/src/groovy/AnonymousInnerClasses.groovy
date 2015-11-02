a175212355d3d7365c48234ee2683e7280916b4d
8uG <155223342@qq.com>
2015-11-02 17:55:55 +0800

    First try with Gradle build.

---
+++ differences/src/groovy/AnonymousInnerClasses.groovy
@@ -0,0 +1,13 @@
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+CountDownLatch called = new CountDownLatch(1)
+
+Timer timer = new Timer()
+timer.schedule(new TimerTask() {
+    void run() {
+        called.countDown()
+    }
+}, 0)
+
+assert called.await(10, TimeUnit.SECONDS)
