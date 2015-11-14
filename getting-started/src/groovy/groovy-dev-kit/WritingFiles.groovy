f46900974aaebbb5d72ba93b11df3755a4a29d09
Will <yzj_000@163.com>
2015-11-14 10:39:13 +0800

    Move project "differences" to a more generic project "getting-started".

---
+++ getting-started/src/groovy/groovy-dev-kit/WritingFiles.groovy
@@ -0,0 +1,7 @@
+baseDir = new File("/tmp")
+
+new File(baseDir,'haiku.txt').withWriter('utf-8') { writer ->
+    writer.writeLine 'Into the ancient pond'
+    writer.writeLine 'A frog jumps'
+    writer.writeLine 'Water’s sound!'
+}
