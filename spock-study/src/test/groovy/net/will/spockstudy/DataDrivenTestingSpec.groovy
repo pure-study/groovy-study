3bbad2dd45f0c146d31bab574a23a75326786497
Will <yydragon.000@gmail.com>
2016-07-02 16:13:46 +0800

    Add project spock-study.

---
+++ spock-study/src/test/groovy/net/will/spockstudy/DataDrivenTestingSpec.groovy
@@ -0,0 +1,35 @@
+package net.will.spockstudy
+
+import groovy.sql.Sql
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class DataDrivenTestingSpec extends Specification {
+    @Shared
+    sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
+    
+    def setupSpec() {
+        sql.execute("create table maxdata (id int primary key, a int, b int, c int)")
+        sql.execute("insert into maxdata values (1, 3, 7, 7), (2, 5, 4, 5), (3, 9, 9, 9)")
+    }
+    
+    def "minimum of #a and #b is #c"() {
+        expect:
+            Math.min(a, b) == c
+        
+        where:
+            a = 3
+            b = Math.random() * 100
+            c = a < b ? a : b
+    }
+    
+    def "maximum of two numbers"() {
+        expect:
+        Math.max(a, b) == c
+    
+        where:
+        [a, b, c] << sql.rows("select a, b, c from maxdata")
+    }
+}
