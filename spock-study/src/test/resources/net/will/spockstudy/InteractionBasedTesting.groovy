0a468cdc881592bfece0454e7036bceff910743c
Will Yuan <zhijun.yuan@ericsson.com>
2016-07-04 17:46:17 +0800

    Some update on Interaction Based Testing in Spock.

--- spock-study/src/test/resources/net/will/spockstudy/InteractionBasedTesting.groovy
+++ spock-study/src/test/resources/net/will/spockstudy/InteractionBasedTesting.groovy
@@ -1,5 +1,7 @@
 package net.will.spockstudy
 
+// http://spockframework.github.io/spock/docs/1.0/interaction_based_testing.html
+
 // Cardinality:
 1 * subscriber.receive("hello")      // exactly one call
 0 * subscriber.receive("hello")      // zero calls
@@ -44,3 +46,30 @@ then:
 _ * auditing._                  // allow any interaction with 'auditing'
 0 * _                           // don't allow any other interaction
 
+// Stubbing
+// Returning Fixed Values:
+subscriber.receive(_) >> "ok"
+
+// Returning Sequences of Values:
+// This will return "ok" for the first invocation, "error" for the second and third invocation, 
+// and "ok" for all remaining invocations.
+subscriber.receive(_) >>> ["ok", "error", "error", "ok"]
+
+// Computing Return Values:
+// the closure delegates to an instance of org.spockframework.mock.IMockInvocation.
+subscriber.receive(_) >> { args -> args[0].size() > 3 ? "ok" : "fail" }
+subscriber.receive(_) >> { String message -> message.size() > 3 ? "ok" : "fail" }
+
+subscriber.receive(_) >> { throw new InternalError("ouch") }
+
+// This will return "ok", "fail", "ok" for the first three invocations, throw InternalError for 
+// the fourth invocations, and return ok for any further invocation.
+subscriber.receive(_) >>> ["ok", "fail", "ok"] >> { throw new InternalError() } >> "ok"
+
+// Combining Mocking and Stubbing:
+// NOTE: Mocking and stubbing of the same method call has to happen in the same interaction.
+1 * subscriber.receive("message1") >> "ok"
+
+// Spies
+// Sometimes, it is desirable to both execute some code and delegate to the real method.
+subscriber.receive(_) >> { String message -> callRealMethod(); message.size() > 3 ? "ok" : "fail" }
