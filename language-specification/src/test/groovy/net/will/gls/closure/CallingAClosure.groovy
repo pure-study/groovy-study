268fd6c702a375a6dd48a687d18cbff83faa5cca
Will Yuan <zhijun.yuan@ericsson.com>
2016-10-27 15:30:00 +0800

    Define a parent project for this repository.

---
+++ language-specification/src/test/groovy/net/will/gls/closure/CallingAClosure.groovy
@@ -0,0 +1,20 @@
+package net.will.gls.closure
+
+import groovy.test.GroovyAssert
+
+import org.junit.Test
+
+class CallingAClosure extends GroovyAssert {
+
+    @Test
+    public void test() {
+        def isOdd = { int i -> i%2 == 1 }
+        assert isOdd(3) == true
+        assert isOdd.call(2) == false
+        
+        def isEven = { it%2 == 0 }
+        assert isEven(3) == false
+        assert isEven.call(2) == true
+    }
+
+}
---
+++ language-specification/src/test/groovy/net/will/gls/closure/ParametersOfClosure.groovy
@@ -0,0 +1,33 @@
+package net.will.gls.closure
+
+import groovy.test.GroovyAssert
+
+import org.junit.Test
+
+class ParametersOfClosure extends GroovyAssert {
+
+    @Test
+    public void test_AcceptsNoArguments() {
+        def magicNumber = { -> 42 }
+        
+        shouldFail {
+            magicNumber(11)
+        }
+    }
+
+    @Test
+    public void test_varargs() {
+        def multiConcat = { int n, String... args ->
+            args.join('') * n
+        }
+        
+        assert multiConcat(2, 'abc','def') == 'abcdefabcdef'
+    }
+
+    @Test
+    public void test_varargs_DeclaredAsAnArray() {
+        def concat2 = { String[] args -> args.join('') }
+        assert concat2('abc', 'def') == 'abcdef'
+    }
+
+}
