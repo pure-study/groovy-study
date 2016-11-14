57c5a7855c24900014f1495f661e57d1084e541a
Will Yuan <zhijun.yuan@ericsson.com>
2016-11-14 14:51:01 +0800

    Tests on DSL.

--- getting-started/src/test/groovy/net/will/ggs/dsl/CommandChainsTest.groovy
+++ getting-started/src/test/groovy/net/will/ggs/dsl/CommandChainsTest.groovy
@@ -1,6 +1,9 @@
 package net.will.ggs.dsl
 
-import org.junit.Test;
+import org.junit.Test
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
 
 import groovy.test.GroovyAssert
 
@@ -12,11 +15,31 @@ class CommandChainsTest extends GroovyAssert {
     }
     
     @Test
-    void test() {
+    void test_exp01() {
         def show = { println it }
         def square_root = { Math.sqrt(it) }
         
+        // equivalent to: please(show).the(square_root).of(100)
         please show the square_root of 100
         assert true
+    }
+    
+    def split(originalString) {
+        [on: { sep ->
+             [trimming: { trimChar ->
+                  Splitter.on(sep).trimResults(CharMatcher.is(trimChar as char)).split(originalString).iterator().toList()
+             }]
+        }]
+    }
+    
+    @Test
+    void test_exp02() {
+        // Splitter.on(',').trimResults(CharMatcher.is('_' as char)).split("_a ,_b_ ,c__").iterator().toList()
+        List result = split "_a ,_b_ ,c__" on ',' trimming '_'
+        println result
+        
+        assert result.contains('a ')
+        assert result.contains('b_ ')
+        assert result.contains('c')
     }
 }
