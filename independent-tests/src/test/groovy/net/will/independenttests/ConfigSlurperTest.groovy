525169187f0d89b90959a19b5e732cfc6fe47d09
Will Yuan <will.yuan@ericsson.com>
2018-04-28 14:29:01 +0800

    Add a set of tests to test ConfigSlurper

---
+++ independent-tests/src/test/groovy/net/will/independenttests/ConfigSlurperTest.groovy
@@ -0,0 +1,41 @@
+package net.will.independenttests
+
+import org.junit.Test
+
+import groovy.test.GroovyAssert
+
+class ConfigSlurperTest extends GroovyAssert {
+    def configContentInMultipleLines = """
+profile {
+    functions {
+       includes = [
+          [module:"m1", function:["f1","f2"]],
+          [module:"m2", function:["f3"]]
+       ]
+       excludes = [
+       ]
+    }
+    groups = [
+    ]
+ }
+        """
+    
+    def configContentInOneLine = """
+profile { functions { includes = [ [module:"m1", function:["f1","f2"]], [module:"m2", function:["f3"]] ] excludes = [ ] } groups = [ ] }
+        """
+    
+    @Test
+    public void testReadConfigFileWithNoError() {
+        def config = new ConfigSlurper().parse(configContentInMultipleLines)
+        
+        assert config?.profile?.functions?.includes?.size() == 2
+        assert config?.profile?.groups?.size() == 0
+    }
+    
+    @Test
+    public void testReadConfigFileWithErrors() {
+        shouldFail {
+            new ConfigSlurper().parse(configContentInOneLine)
+        }
+    }
+}
